4f624942c7139c6d5b49b1825ec6dbfefa0e4601
MilyCat353 <146204164+MilyCat353@users.noreply.github.com>
2025-06-30 12:20:05 -0300

    Update telaCliente.py

--- banco/telaCliente.py
+++ banco/telaCliente.py
@@ -1,27 +1,45 @@
 import getpass
 import csv
 from conta import Conta
+
+#Inicialização de variaveis
+
 contas = []
-with open('contas.csv', newline = '', encoding = 'utf-8', errors = 'ignore') as lerContas:
-    leitor = csv.reader(lerContas, delimiter = ',')
-    for linha in leitor:
-        conta = Conta(int(linha[0]), int(linha[1]), linha[2], float(linha[3]), linha[4])
-        contas.append(conta)
-print (contas)
+
+acesso_liberado = False
+contaEncontrada = None
 
 agencia = int(input("Digite a agẽncia: "))
 numero_conta = int(input("Digite sua conta corente: "))
 senha = getpass.getpass("Digite a semha: ")
 
-contaEncontrada = ''
-for conta in contas:
-    if numero_conta == conta.numero:
-        contaEncontrada = Conta(conta.agencia, conta.numero, conta.titular, conta.saldo, conta.senha)
-if contaEncontrada == None:
-    print("Dacos incorretos")
-else:
-    print(contaEncontrada.senha)
-    if senha == contaEncontrada.senha and agencia == contaEncontrada.agencia:
-        print("Acesso liberado")
-    else:
+def lerArquivo():
+    with open('contas.csv', newline = '', encoding = 'utf-8', errors = 'ignore') as lerContas:
+        leitor = csv.reader(lerContas, delimiter = ',')
+        for linha in leitor:
+            conta = Conta(int(linha[0]), int(linha[1]), linha[2], float(linha[3]), linha[4])
+            contas.append(conta)
+
+def encontrarContaCorrente():
+
+    global contaEncontrada
+    for conta in contas:
+        if numero_conta == conta.numero:
+            contaEncontrada = Conta(conta.agencia, conta.numero, conta.titular, conta.saldo, conta.senha)
+
+def verificarAcesso():
+    global contaEncontrada
+    if contaEncontrada == None:
         print("Dados incorretos")
+        print("Dacos incorretos")
+    else:
+        print(contaEncontrada.senha)
+        if senha == contaEncontrada.senha and agencia == contaEncontrada.agencia:
+            print("Acesso liberado")
+        else:
+            print("Dados incorretos")
+
+
+lerArquivo()
+encontrarContaCorrente()
+verificarAcesso()
